6928c5480dcc319258a6060b678a2b08443bf9fb
jasonrenau <jasondu40160@gmail.com>
2023-06-01 13:57:29 +0200

    new commit

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const openmod = document.querySelector('.btn-box');
 const closemod = document.querySelector('.btn-close');
 // modal
 const modal = document.querySelector('.modal-overlay');
+// reste de la page
+const hero = document.querySelector('.hero');
 
 openmod.addEventListener('click', function () {
   modal.classList.add('open-modal');
 });
+
 closemod.addEventListener('click', function () {
   modal.classList.remove('open-modal');
+});
+
+window.addEventListener('click', function (e) {
+  if (e.target == modal) {
+    modal.classList.remove('open-modal');
+  }
 });
